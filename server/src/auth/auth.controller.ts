c128378a07f3e28933b540f3722e98dd327c99b7
dmtr35 <nuke-id@ukr.net>
2022-09-25 13:47:27 +0300

    auth

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Post } from '@nestjs/common'
+import { CreateUserDto } from '../users/dto/create-user.dto'
+import { AuthService } from './auth.service'
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger"
+
+
+@ApiTags('Авторизация')
+@Controller('auth')
+export class AuthController {
+
+    constructor(private authService: AuthService) { }
+
+    @Post('/login')
+    login(@Body() userDto: CreateUserDto) {
+        return this.authService.login(userDto)
+    }
+
+    @Post('/registration')
+    register(@Body() userDto: CreateUserDto) {
+        return this.authService.register(userDto)
+    }
+
+
+}
+
+
+
+
---
+++ server/src/users/user.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import mongoose from 'mongoose'
+import { Document } from 'mongoose'
+import { ApiProperty } from "@nestjs/swagger"
+// import { ObjectID } from 'bson';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @ApiProperty({ example: '1', description: 'id пользователя' })
+  _id: mongoose.Types.ObjectId
+
+  @ApiProperty({ example: 'user@gmail.com', description: 'почтовый адрес' })
+  @Prop({ unique: true, required: true })
+  email: string
+
+  @ApiProperty({ example: '12345', description: 'Пароль' })
+  @Prop({ required: true })
+  password: string
+
+  // @ApiProperty({example: 'true', description: 'Забанен или нет'})
+  @Prop({ default: false })
+  banned: boolean
+
+  // @ApiProperty({example: 'Бот', description: 'Причина бана'})
+  @Prop()
+  banReason: string
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
+
+
